d9e74a8db6374e8c0c1db9894ebb0964d904320b
Pedro Henrique <phcamargo23@gmail.com>
2017-04-24 00:24:27 -0300

    commit inicial

---
+++ popup.js
@@ -0,0 +1,5 @@
+        $(document).ready(function(){
+            $.each(chrome.extension.getBackgroundPage().GB.getBlockedSites(), function (index, value) {
+                $("#blockedlist").append("<div class='siterow' title='"+value+"'><div class='sitename'>"+index+"</div><span class='sitedesc'> : "+value+"</span></div>");
+            });
+        });
