8f5a5833b9ae59b18a9dbb8f33e9892dfea51d21
Pedro Henrique <phcamargo23@gmail.com>
2017-06-20 17:54:35 -0300

    -

--- js/popup.js
+++ js/popup.js
@@ -3,26 +3,10 @@ $(document).ready(function () {
     //     $("#blockedlist").append("<div class='siterow' title='"+value+"'><div class='sitename'>"+index+"</div><span class='sitedesc'> : "+value+"</span></div>");
     // });
 
-    // Read it using the storage API
-
-    function dump(obj) {
-        var out = '';
-        for (var i in obj) {
-            out += i + ": " + obj[i] + "\n";
-        }
-
-        // alert(out);
-
-        // or, if you wanted to avoid alerts...
-
-        var pre = document.createElement('pre');
-        pre.innerHTML = out;
-        $("#words").appendChild(pre)
-    }
-
-
     chrome.storage.sync.get('historico', function (items) {
-        dump(items['historico']);
+        Object.values(items["historico"]).forEach(function (element) {
+            console.log(element);
+        }, this);
     });
 
 });
