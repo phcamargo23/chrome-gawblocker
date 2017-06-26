25950c024561f9c57ac19996f220fe5be9e0c87b
Pedro Henrique <phcamargo23@gmail.com>
2017-06-26 16:15:15 -0300

    Inicou implementação front-end utilizando o Materialize.

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,9 @@
-$(document).ready(function () {
+$("document").ready(function(){
     // $.each(chrome.extension.getBackgroundPage().GB.getBlockedSites(), function (index, value) {
     //     $("#blockedlist").append("<div class='siterow' title='"+value+"'><div class='sitename'>"+index+"</div><span class='sitedesc'> : "+value+"</span></div>");
     // });
+
+    $(".button-collapse").sideNav();
 
     chrome.storage.sync.get('historico', function (items) {
         Object.values(items["historico"]).forEach(function (element) {
